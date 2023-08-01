fff258ec5a7048a197d1714550b32fa81cedfda1
FelipePardim <felipe.santos@ciss.com.br>
2023-08-01 16:24:11 -0300

    feat: adding input login component

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,26 +1,33 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
+import { InputLogin } from "./components/InputLogin";
 
 export const Login = () => {
+    const inputPasswordRef = useRef<HTMLInputElement>(null);
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleEntrar = () => {
         console.log(email, password);
-        
     }
 
     return (
         <div>
             <form action="">
-                <label htmlFor="">
-                    <span>Email</span>
-                    <input type="text" onChange={e => setEmail(e.target.value)}/>
-                </label>
+                 <InputLogin 
+                    label="Email"
+                    value={email}
+                    onChange={newValue => setEmail(newValue)}
+                    onPressEnter={() => inputPasswordRef.current?.focus()}
+                 />
 
-                <label htmlFor="">
-                    <span>Senha</span>
-                    <input type="password" onChange={e => setPassword(e.target.value)}/>
-                </label>
+                <InputLogin
+                    type="password"
+                    label="Senha"
+                    value={password}
+                    ref={inputPasswordRef}
+                    onChange={newValue => setPassword(newValue)}
+                 />
 
                 <button type="button" onClick={handleEntrar}>
                     Entrar
