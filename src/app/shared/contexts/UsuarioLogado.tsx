bf70e01cce3635f5bbf4fa8d453df0e4825b80c5
FelipePardim <felipe.santos@ciss.com.br>
2023-08-02 15:33:35 -0300

    feat: passing state to the custom react hook

--- src/app/shared/contexts/UsuarioLogado.tsx
+++ src/app/shared/contexts/UsuarioLogado.tsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 interface IUsuarioLogadoContextData {
     nomeDoUsuario: string;
@@ -13,11 +13,20 @@ export const UsuarioLogadoContext = createContext<IUsuarioLogadoContextData>({} 
 
 export const UsuarioLogadoProvider: React.FC<IUsuarioLogadoProvider> = ({ children }) => {
 
+    const [nome, setNome] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {
+            setNome('Teste');
+        }, 300)
+    })
+
     const handleLogout = useCallback(() => {
         console.log("Usuário fez logout");
     }, []);
+
     return (
-        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: 'Usuário', logout: handleLogout }}>
+        <UsuarioLogadoContext.Provider value={{ nomeDoUsuario: nome, logout: handleLogout }}>
             {children}
         </UsuarioLogadoContext.Provider>
     )
