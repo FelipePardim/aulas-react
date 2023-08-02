419c86c33c74d85342d67fbc05ecf9261dc153a0
FelipePardim <felipe.santos@ciss.com.br>
2023-08-02 11:13:38 -0300

    feat: Adding custom login button

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react"
 import { InputLogin } from "./components/InputLogin";
+import { ButtonLogin } from "./components/ButtonLogin";
 
 export const Login = () => {
     const inputPasswordRef = useRef<HTMLInputElement>(null);
@@ -29,9 +30,16 @@ export const Login = () => {
                     onChange={newValue => setPassword(newValue)}
                  />
 
-                <button type="button" onClick={handleEntrar}>
+                {/* <button type="button" onClick={handleEntrar}>
                     Entrar
-                </button>
+                </button> */}
+
+                <ButtonLogin type="button" onClick={handleEntrar}>
+                    Entrar
+                </ButtonLogin>
+                <ButtonLogin type="button" onClick={handleEntrar}>
+                    Cadastrar
+                </ButtonLogin>
             </form>
         </div>
     )
